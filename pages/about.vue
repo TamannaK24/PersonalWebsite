da440a430fd1204b2677c715dd02f02cf9785bd5
tamannak24 <tamanna122404@gmail.com>
2025-07-09 15:03:48 -0500

    blue box

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,12 @@
     <div>
         <Navbar /> 
         <h2> about</h2>
+
+        <div class="relative h-50 w-50 ">
+            <div class="absolute inset-y-0 right-20 bg-blue-600 box-border h-32 w-32 p-10">
+                <h1> hello </h1>
+            </div>
+        </div>
     </div>
 </template>
 
